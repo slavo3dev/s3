a776e607644182975d11b049838fed29d0ea9dbb
Slavo Popovic <slavo@slavo3.com>
2022-07-14 18:05:08 -0400

    adding new project

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Post: NextPage = () => {
+	return <h1>Post Page</h1>;
+};
+
+export default Post;
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Blog: NextPage = () => {
+	return <h1>Blog Page</h1>;
+};
+
+export default Blog;
